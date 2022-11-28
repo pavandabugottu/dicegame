a1e361475a3d54d3a9caf45ed9da554da7a3f929
Pavan Dabbugottu <42940547+pavandabugottu@users.noreply.github.com>
2022-11-28 00:07:52 -0800

    initial commit
    dice game initial changes

---
+++ index.js
@@ -0,0 +1,19 @@
+function rollDices() {
+    var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+    var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+    document.querySelector(".img1").setAttribute("src", `images/dice${randomNumber1}.png`);
+    document.querySelector(".img2").setAttribute("src", `images/dice${randomNumber2}.png`);
+
+    var player1Value = document.querySelector(".img1").getAttribute("src");
+    var player2Value = document.querySelector(".img2").getAttribute("src");
+
+    if (player1Value === player2Value) {
+        document.querySelector(".container h1").innerHTML = "Draw!";
+    }
+    else if (player1Value > player2Value) {
+        document.querySelector(".container h1").innerHTML = "Dabbu Wins!";
+    }
+    else {
+        document.querySelector(".container h1").innerHTML = "Pillu Wins!";
+    }
+}
